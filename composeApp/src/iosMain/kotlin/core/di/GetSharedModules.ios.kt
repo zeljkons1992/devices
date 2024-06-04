e39d2e3faac2e75f54d8c1ee422b6017f21b3cf5
nikola.rankovic <nikolarankovic2810@gmail.com>
2024-06-04 13:59:23 +0200

    dev test initialization

--- composeApp/src/iosMain/kotlin/core/di/GetSharedModules.ios.kt
+++ composeApp/src/iosMain/kotlin/core/di/GetSharedModules.ios.kt
@@ -1,15 +1,13 @@
-import com.russhwolf.settings.Settings
 import core.network.ApiService
 import core.network.provideHttpClient
 import features.auth.data.repository.AuthRepositoryImpl
 import features.auth.domain.repository.AuthRepository
 import features.auth.domain.usecase.RegisterUserUseCase
 import features.auth.presentation.mvi.viewmodel.AuthViewModel
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val viewModelModule = module {
-    single { AuthViewModel(get(), get()) }
+    single { AuthViewModel(get(), get(), get()) }
 }
 
 actual val useCaseModule = module {
@@ -22,8 +20,9 @@ actual val repositoryModule = module {
 
 actual val apiModule = module {
     single { provideHttpClient() }
-    single { ApiService(get(),"") }
+    single { ApiService(get(), "") }
 }
 
-actual val dataStoreModule: Module
-    get() = TODO("Not yet implemented")
+actual val dataStoreModule = module {
+
+}
