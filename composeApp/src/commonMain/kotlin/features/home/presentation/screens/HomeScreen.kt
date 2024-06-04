9db286f90923447ef8b484e75147fabbe10f5050
nikola.rankovic <nikolarankovic2810@gmail.com>
2024-06-04 14:37:42 +0200

    added logout button

--- composeApp/src/commonMain/kotlin/features/home/presentation/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/features/home/presentation/screens/HomeScreen.kt
@@ -1,33 +1,40 @@
 package features.home.presentation.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import core.utils.datastore.DataStoreRepository
+import features.auth.presentaion.screens.login.LoginScreen
 import org.koin.compose.koinInject
 
 class HomeScreen : Screen {
 
     @Composable
-    override fun Content( ) {
+    override fun Content() {
         val navigator = LocalNavigator.current
-        val dataStoreRepository: DataStoreRepository= koinInject()
-        Box(
+        val dataStoreRepository: DataStoreRepository = koinInject()
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.Center
         ) {
             Button(onClick = {
-                val jwt =dataStoreRepository.getString("jwtToken")
+                val jwt = dataStoreRepository.getString("jwtToken")
                 println("JWT TOKEN :$jwt")
                 navigator?.push(HomeScreenDetails(number = 10))
             }) {
                 Text("GO")
+            }
+            Button(onClick = {
+                dataStoreRepository.deleteString("jwtToken")
+                navigator?.replace(LoginScreen())
+            }) {
+                Text("LOGOUT")
             }
         }
     }
